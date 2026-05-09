f174ab7c4c6de0af96115cff48169b9a793101a8
Daniel Medeiros <danielmmf@gmail.com>
2026-05-09 17:17:02 -0300

    chore: enable firebase realtime config

--- viabilidade-verde/firebase.config.js
+++ viabilidade-verde/firebase.config.js
@@ -8,13 +8,14 @@
  * Para fallback local:
  * - deixe VV_FIREBASE_ENABLED como false.
  */
-window.VV_FIREBASE_ENABLED = false;
+window.VV_FIREBASE_ENABLED = true;
 window.VV_FIREBASE_CONFIG = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+  apiKey: 'AIzaSyBBx1AvjAGmY_kHrJ2I5wNe8YEC6UM79fU',
+  authDomain: 'emais-energia.firebaseapp.com',
+  databaseURL: 'https://emais-energia-default-rtdb.firebaseio.com',
+  projectId: 'emais-energia',
+  storageBucket: 'emais-energia.firebasestorage.app',
+  messagingSenderId: '389978309074',
+  appId: '1:389978309074:web:c3c3eca9b2a121ef0bc80b',
+  measurementId: 'G-B4M6N5DLNK'
 };
